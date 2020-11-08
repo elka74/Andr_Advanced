8705fd9257098f577a9348a16c3f0410be1c4b23
mikhe <mikheeva@1454.ru>
2020-11-08 23:47:05 +0300

    в файлах xml при сборке все время пищет ошибки, то нет ресурсов, то данный ресурс здесь не может находиться. Я делала все по видеоуроку.

--- app/src/main/java/com/example/andr_advanced/MainActivity.java
+++ app/src/main/java/com/example/andr_advanced/MainActivity.java
@@ -1,27 +1,22 @@
 package com.example.andr_advanced;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.ListAdapter;
-import android.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    private ListAdapter listAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = initToolbar();
-        initDrawer(toolbar);
-        initList();
-
 
         Home_fragment fragment = new Home_fragment();
         getSupportFragmentManager()
@@ -29,16 +24,27 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 .replace(R.id.frg_home,fragment)
                 .commit();
 
+        Toolbar toolbar = initToolbar();
+        initDrawer(toolbar);
 
+    }
+
+    private Toolbar initToolbar(){
+        return findViewById(R.id.toolbar);
+    }
+    private void initDrawer(Toolbar toolbar){
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+       ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+       drawer.addDrawerListener(toggle);
+       toggle.syncState();
+       navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         return false;
     }
-    private Toolbar initToolbar(){
-
-    }
-
-
 }
