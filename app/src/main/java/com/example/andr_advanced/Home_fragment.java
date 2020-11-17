2c53f8e4e21c42af510a8815aa2d156cbfaf0047
mikhe <mikheeva@1454.ru>
2020-11-17 15:34:49 +0300

    домашка с выход в интернет

--- app/src/main/java/com/example/andr_advanced/Home_fragment.java
+++ app/src/main/java/com/example/andr_advanced/Home_fragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TextView;
@@ -13,63 +15,42 @@ import androidx.fragment.app.Fragment;
 
 
 public class Home_fragment extends Fragment {
-     public static  OnDialogListener dialogListener;
+    public static OnDialogListener dialogListener;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View frgm = inflater.inflate(R.layout.fragment_home_fragment, container, false);
         TextView text = frgm.findViewById(R.id.text);
-        TextView tx_city = frgm.findViewById(R.id.tx_mos);
+        EditText tx_city = frgm.findViewById(R.id.tx_mos);
         ImageView image = frgm.findViewById(R.id.image);
-        TextView textView = frgm.findViewById(R.id.textView);
+        EditText temp = frgm.findViewById(R.id.textView);
         TableLayout dop_fun = frgm.findViewById(R.id.dop_fun);
-        TextView city = frgm.findViewById(R.id.city);
+        Button refresh = frgm.findViewById(R.id.city);
         TextView textView1 = frgm.findViewById(R.id.textView2);
-        TextView textView2 = frgm.findViewById(R.id.textView3);
+        EditText wind = frgm.findViewById(R.id.textView3);
         TextView textView3 = frgm.findViewById(R.id.textView4);
-        TextView textView4 = frgm.findViewById(R.id.textView5);
+        EditText humidity = frgm.findViewById(R.id.textView5);
         TextView textView5 = frgm.findViewById(R.id.textView6);
-        TextView textView6 = frgm.findViewById(R.id.textView7);
+        EditText pressure = frgm.findViewById(R.id.textView7);
+        EditText editText = frgm.findViewById(R.id.input );
 
 
-
-       /* city.setOnClickListener(v -> {
-            Choose_City fragment = new Choose_City();
-            assert getFragmentManager() != null;
-            getFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.frg_home, fragment)
-                    .commit();
-        });
-        return frgm;*/
-
-
-        city.setOnClickListener(view -> {
+        /*city.setOnClickListener(view -> {
             Dop_fragment dialogFragment =
                     Dop_fragment.newInstance();
             dialogFragment.setOnDialogListener(dialogListener);
+            assert getFragmentManager() != null;
             dialogFragment.show(getFragmentManager(),
                     "dop_fragment");
-        });
+        });*/
         return frgm;
     }
 
 
-
-    @SuppressLint("NonConstantResourceId")
-    int translateIdToIndex(int id){
-        int index = -1;
-        switch (id){
-            case R.id.btnOk:
-                index = 1;
-                break;
-            case R.id.btnNo:
-                index = 2;
-                break;
-        }
-        return index;
-    }
+}
 
 
 
-    }
+
+
+
--- app/src/main/java/com/example/andr_advanced/MainActivity.java
+++ app/src/main/java/com/example/andr_advanced/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.andr_advanced;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+
+import com.example.andr_advanced.model.WeatherRequest;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.annotation.NonNull;
@@ -8,21 +11,50 @@ import androidx.appcompat.widget.SearchView;
 import androidx.core.view.GravityCompat;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 
+import android.os.Handler;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.gson.Gson;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+import javax.net.ssl.HttpsURLConnection;
 
+import static com.example.andr_advanced.BuildConfig.WEATHER_API_KEY;
+import static com.example.andr_advanced.Home_fragment.dialogListener;
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private static final String TAG = "WEATHER";
+    private static final String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=f370763538b29033cdb879554029b1b5";
+    private static final String WEATHER_API_KEY = "f370763538b29033cdb879554029b1b5";
+    private EditText city;
+    private EditText temp;
+    private EditText pressure;
+    private EditText humidity;
+    private EditText windSpeed;
+    Dop_fragment frg;
+    Home_fragment fragment;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +63,88 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         Toolbar toolbar = initToolbar();
         initDrawer(toolbar);
-
         Home_fragment fragment = new Home_fragment();
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.frg_home, fragment)
                 .commit();
+        city = (EditText) findViewById(R.id.tx_mos);
+        temp = (EditText) findViewById(R.id.textView);
+        windSpeed = (EditText) findViewById(R.id.textView3);
+        humidity = (EditText) findViewById(R.id.textView5);
+        pressure = (EditText) findViewById(R.id.textView7);
+        Button btn = (Button) findViewById(R.id.city);
+        btn.setOnClickListener(clickListener);
 
     }
 
+
+    View.OnClickListener clickListener = new View.OnClickListener() {
+        private WeatherRequest weatherRequest;
+
+        @Override
+        public void onClick(View v) {
+            try {
+                //final String url = String.format("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s",
+                //(editText = (EditText) findViewById(R.id.input)).getText(),
+                // BuildConfig.WEATHER_API_KEY);
+                final URL uri = new URL(WEATHER_URL);
+                final Handler handler = new Handler(); // Запоминаем основной поток
+                new Thread(
+                        new Runnable() {
+
+                            @Override
+                            public void run() {
+                                HttpsURLConnection urlConnection = null;
+                                try {
+                                    urlConnection = (HttpsURLConnection) uri.openConnection();
+                                    urlConnection.setRequestMethod("GET"); // установка метода получения данных -GET
+                                    urlConnection.setReadTimeout(10000); // установка таймаута - 10 000 миллисекунд
+                                    BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream())); // читаем  данные в поток
+                                    String result = getLines(in);
+                                    // преобразование данных запроса в модель
+                                    Gson gson = new Gson();
+                                    final WeatherRequest weatherRequest = gson.fromJson(result, WeatherRequest.class);
+                                    // Возвращаемся к основному потоку
+                                    handler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+
+                                            displayWeather(weatherRequest);
+                                        }
+                                    });
+
+                                } catch (Exception e) {
+                                    Log.e(TAG, "Fail connection", e);
+                                    e.printStackTrace();
+                                } finally {
+                                    if (null != urlConnection) {
+                                        urlConnection.disconnect();
+                                    }
+                                }
+                            }
+                        }
+                ).start();
+            } catch (MalformedURLException e) {
+                Log.e(TAG, "Fail URI", e);
+                e.printStackTrace();
+            }
+        }
+
+        private String getLines(BufferedReader in) {
+            return in.lines().collect(Collectors.joining("\n"));
+        }
+
+        @SuppressLint("DefaultLocale")
+        private void displayWeather(WeatherRequest weatherRequest) {
+            this.weatherRequest = weatherRequest;
+            city.setText(weatherRequest.getName());
+            temp.setText(String.format("%f2", weatherRequest.getMain().getTemp()));
+            pressure.setText(String.format("%d", weatherRequest.getMain().getPressure()));
+            humidity.setText(String.format("%d", weatherRequest.getMain().getHumidity()));
+            windSpeed.setText(String.format("%d", weatherRequest.getWind().getSpeed()));
+        }
+    };
 
 
     private Toolbar initToolbar() {
@@ -59,6 +164,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
     }
+
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -80,6 +186,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 Snackbar.make(searchText, query, Snackbar.LENGTH_LONG).show();
                 return true;
             }
+
             @Override
             public boolean onQueryTextChange(String newText) {
                 return true;
@@ -98,6 +205,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         return super.onOptionsItemSelected(item);
     }
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
@@ -118,11 +226,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         } else if (id == R.id.choose_city_menu) {
             return true;
-
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
+
